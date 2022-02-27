7cbb42b65c1580070151b066d6b318f08432bfc6
amankrokx <amankumar.spj410@gmail.com>
2022-02-27 14:05:46 +0530

    Working ip weather

---
+++ main.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+struct string {
+  char *ptr;
+  size_t len;
+};
+
+typedef struct string ST;
+
+void init_string(struct string *s) {
+  s->len = 0;
+  s->ptr = malloc(s->len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  s->ptr[0] = '\0';
+}
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s) {
+  size_t new_len = s->len + size*nmemb;
+  s->ptr = realloc(s->ptr, new_len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr+s->len, ptr, size*nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
+
+  return size*nmemb;
+}
+
+char* urlencode(char* originalText) {
+    // allocate memory for the worst possible case (all characters need to be encoded)
+    char *encodedText = (char *)malloc(sizeof(char)*strlen(originalText)*2);
+    int pos = 0;
+    for (size_t i = 0; i < strlen(originalText); i++) {
+        if (originalText[i] == '&') {
+            encodedText[pos++] = '\\';
+        }
+        encodedText[pos++] = originalText[i];
+    }
+    encodedText[pos] = '\0';
+    return encodedText;
+}
+
+char * getvalue( char * json, char * key, int occ, char * loc ) {
+  char * start = strstr(json, key) + strlen(key);
+  while (occ > 1) {
+    char * t = strstr(start, key) + strlen(key);
+    occ--;
+    if ( t ) start = t;
+    else break;
+  }
+
+  int k = 0;
+  for (int i = 0; start[i] != ',' && start[i] != '}'; i++) {
+    switch (start[i]) {
+      case ' ':
+      case '\"':
+      case '{':
+      case ':':
+        //printf("skipped : %c\n", start[i]);
+        continue;
+        break;
+    }
+    //printf("%c", start[i]);
+    loc[k++] = start[i];
+  }
+  loc[k] = '\0';
+  return loc;
+}
+
+ST getreq(char * ur) {
+    CURL *curl;
+    CURLcode response;
+    struct string s;
+    init_string(&s);
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, ur);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);  
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+
+        response = curl_easy_perform(curl);
+
+        if(response != CURLE_OK) {
+            fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(response));
+            strcpy(s.ptr, curl_easy_strerror(response));
+            return s;
+        } /*else {
+            printf("Data : \n%s", s.ptr);
+        }*/
+        curl_easy_cleanup(curl);
+    }
+    return s;
+}
+
+int main () {
+  char * buff = (char *) malloc(1024);
+  //ST weatheratip = getreq("https://api.openweathermap.org/data/2.5/weather?lat=12.9634&lon=77.5855&appid=b1741d4b9a702da375fddc8571ba4eaa");
+  ST ip = getreq("http://ip-api.com/json");
+  char * lat = (char *) malloc(12);
+  char * lon = (char *) malloc(12);
+  lat = getvalue(ip.ptr, "\"lat\"", 1, lat);
+  lon = getvalue(ip.ptr, "\"lon\"", 1, lon);
+  sprintf(buff, "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=b1741d4b9a702da375fddc8571ba4eaa", lat, lon);
+  ST weatheratip = getreq(buff);
+  char * desc = (char *) malloc(15);
+  char * temp = (char *) malloc(15);
+  char * pressure = (char *) malloc(15);
+  desc = getvalue(weatheratip.ptr, "\"description\"", 1, desc);
+  temp = getvalue(weatheratip.ptr, "\"temp\"", 1, temp);
+  pressure = getvalue(weatheratip.ptr, "\"pressure\"", 1, pressure);
+  printf("\nWeather : %s\nTemperature: %s\nPressure : %s\n\n", desc, temp, pressure);
+
+  free(ip.ptr);
+  free(weatheratip.ptr);
+  
+  //out = getvalue( json, s, o, out);
+  //printf("\n value : %s\n", out);
+  return 0;
+}
